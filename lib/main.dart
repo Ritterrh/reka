0de6a629bf142e58da4c4ff3160c435b3cab03c3
Ritterrh <110771711+Ritterrh@users.noreply.github.com>
2023-12-28 11:24:25 +0100

    Neu Struckierung

--- lib/main.dart
+++ lib/main.dart
@@ -40,7 +40,6 @@ Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await setupServiceLocator();
-  PackageInfo packageInfo = await PackageInfo.fromPlatform();
   requestLocationPermission();
   NewsData.updateDataPeriodically();
   PachtData.updateDataPeriodically();
